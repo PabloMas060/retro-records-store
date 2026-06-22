2c313dcc34e549559c63ae2b24aecc3c1cb3ba8b
pablo.mas <pablo.mas.dev@gmail.com>
2026-06-22 06:19:23 -0300

    config prod

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,21 +6,24 @@ module.exports = {
     password: process.env.DB_PASSWORD_DEV,
     database: process.env.DB_DATABASE_DEV,
     host: process.env.DB_HOST_DEV,
-    port : process.env.DB_PORT_DEV,
-    dialect: "mysql",
+    port: process.env.DB_PORT_DEV,
+    dialect: 'mysql'
   },
+
   test: {
-    username: "root",
+    username: 'root',
     password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
+
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
+    port: process.env.DB_PORT_PROD,
+    dialect: 'mysql'
+  }
 };
